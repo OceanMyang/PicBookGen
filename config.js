ced28a4525013b3843c7bb81358803702eb55d65
OceanZ <my18702189114@gmail.com>
2024-03-07 15:47:16 -0800

    Serve HTML and script

--- config.js
+++ config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const fs = require('fs');
+const fsp = require('fs').promises;
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 const PATH_INDEX = path.join(__dirname, 'src', 'views', 'index.html');
 const PATH_FILES = path.join(__dirname, 'public', 'files')
+
+const readdirAsync = (path) => {
+    let files = [];
+    (async () => {
+    const data = await fsp.readdir(path)
+    files = data;
+    })();
+    return files;
+}
+
+const readFileAsync = (path) => {
+    let file = null;
+    (async () => {
+        const data = await fsp.readFile(path)
+        file = data;
+    })()
+    return file;
+}
 
 module.exports = {
     hostname,
--- server.js
+++ server.js
@@ -1,43 +1,66 @@
-const {
-  hostname,
-  port, 
-  PATH_INDEX,
-  PATH_FILES,
-} = require('./config')
+const { hostname, port, PATH_INDEX, PATH_FILES } = require("./config");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const fsp = require("fs").promises;
+const path = require("path");
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-let files = [];
-fs.readdir(PATH_FILES, (err, data)=>{
-  if (err) throw err;
-  console.log(data);
-  files = data;
-})
-
-app.get('/', (req, res)=>{
+app.get("/", async (req, res) => {
+  const files = await fsp.readdir(PATH_FILES, "utf-8");
   const filenames = files.map((file) => path.parse(file).name);
-  console.log(filenames);
-  
-  res.render('index', {
-    filenames: filenames
-  }, (err, html)=>{
-    if (err) throw (err);
+
+  const exists = files.length !== 0;
+  const header = exists ? "index" : "empty";
+  let locals = {};
+  locals.filenames = exists ? filenames : null;
+
+  res.render(header, locals, (err, html) => {
+    if (err) throw err;
     res.send(html);
   });
 });
 
-process.on('uncaughtException', err => {
-  console.error('Error: ', err);
-  process.exit(1);
-})
+app.get("/:name", async (req, res) => {
+  const filename = req.params.name;
+  const filepath = path.join(__dirname, "public", "files", `${filename}.html`);
+  const scriptpath = path.join(__dirname, "public", "js", `editor.js`);
+
+  let header = '';
+  let locals = {};
+
+  try {
+    const file = await fsp.readFile(filepath, "utf-8");
+    console.log(file);
+    const script = await fsp.readFile(scriptpath, "utf-8");
+    console.log(script);
+
+    header = 'editor';
+    locals.file = file;
+    locals.script = "<script>\n" + script + "\n</script>";
+  } catch (err) {
+    header = 'error';
+    file = '404 File Not Found';
+    throw err;
+  }
+  
+  res.render(
+    header,
+    locals,
+    (err, html) => {
+      if (err) throw err;
+      res.send(html);
+    }
+  );
+});
+
+process.on("uncaughtException", (err) => {
+  console.error(err);
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
