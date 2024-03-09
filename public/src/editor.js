78d7da74196f367f208ed2309e0e8ea1b436162f
OceanZ <my18702189114@gmail.com>
2024-03-09 12:48:32 -0800

    title & new file

--- public/src/editor.js
+++ public/src/editor.js
@@ -4,8 +4,11 @@ $("#editor").on("mouseup", () => {
   if (selectedText) {
     const range = selection.getRangeAt(0);
     if (range.startContainer === range.endContainer) {
-      const anchor = $('<a href="images/mac.jpeg"></a>');
-      const span = $("<span></span>").text(selectedText);
+      const anchor = $('<a/>', {
+        href: "images/mac.jpeg",
+        // style: "pointer-events: none"
+      });
+      const span = $("<span/>").html(selectedText);
       anchor.append(span);
       range.deleteContents();
       range.insertNode($(anchor)[0]);
@@ -13,15 +16,35 @@ $("#editor").on("mouseup", () => {
   }
 });
 
-$("#open-file").on("change", (e) => {
-  const chosenfile = e.target.files ? e.target.files[0] : null;
-
-  if (chosenfile) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      file = e.target.result;
-      $("#editor").html(file);
-    };
-    reader.readAsText(chosenfile);
-  }
+$("#open-file-local").on("click", () => {
+  const input = $('<input/>', {
+    type: 'file',
+    style: 'display: none;'
+  });
+  input.on('change', (e) => {
+    const chosenfile = e.target.files ? e.target.files[0] : null;
+    if (chosenfile) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        file = e.target.result;
+        $("#editor").text(file);
+      };
+      reader.readAsText(chosenfile);
+    }
+  });
+  input.trigger('click');
 });
+
+$("#save-file").on("click", async () => {
+  // const filename = $("#file-name").text();
+  // const file = $("#editor").html();
+  // const response = await fetch('/', {
+  //   method: "POST",
+  //   mode: "same-origin",
+  //   body: {
+  //     filename: filename,
+  //     file: JSON.stringify(file),
+  //   },
+  // })
+  // console.log(response);
+})
